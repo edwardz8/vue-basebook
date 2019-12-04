edcec3a95ae7b92aab96b30ff2cb95c5996087e2
edwardz8 <tolklorist@gmail.com>
2019-12-04 16:51:31 -0600

    firestore add vuexfire and cloudfire integration

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="container">
     <p>Profile Page</p>
-    <div>
-      <div v-for="fav in favorites" :key="fav.id">
-        <span>{{ fav.Name }}</span>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -15,6 +10,11 @@ import firebase from "firebase";
 
 export default {
   name: "Profile",
+  data() {
+    return {
+      favorites: []
+    };
+  },
   computed: {
     // map this.user to this.$store.getters.user
     ...mapGetters({
